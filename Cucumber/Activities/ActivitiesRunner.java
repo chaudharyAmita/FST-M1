bb9a37e26074c57e8633bec3b5c63f0d56a14ea9
chaudharyAmita <amita.chaudhary1@ibm.com>
2024-08-02 17:47:26 +0530

    Add files via upload

---
+++ Cucumber/Activities/ActivitiesRunner.java
@@ -0,0 +1,21 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "Features",
+    glue = {"stepDefinitions"},
+    tags = "@SimpleAlert",
+    plugin = {"pretty"},
+    //plugin = {"html: test-reports"},
+    //plugin = {"json: test-reports/json-report.json"},
+    monochrome = true
+)
+
+public class ActivitiesRunner {
+    //This is intentionally blank
+}
